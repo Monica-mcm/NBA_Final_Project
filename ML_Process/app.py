81e518d759be2eb52399d6390fea57f2b33317af
Joshua Castillo <joshuacastillod@gmail.com>
2020-06-08 19:22:12 -0500

    Test API

--- ML_Process/app.py
+++ ML_Process/app.py
@@ -3,27 +3,27 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('finalized_model.pkl', 'rb'))
+model = pickle.load(open('finalized_model_2.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('homepage.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
 
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    #prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    #output = round(prediction[0], 2)
 
     return render_template('machinelearning.html', prediction_text='The player will be {} MVP'.format(output))
 
 @app.route('/plots')
 def plots():
 
-    return render_template('Plots_Javascript\index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
