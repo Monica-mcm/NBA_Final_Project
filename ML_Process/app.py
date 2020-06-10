14630e6ca3c7534d4f2a62fcbcd8ad73c28a196a
Monica Mateos <monicamc92@gmail.com>
2020-06-09 20:30:42 -0500

    NN_Work with outputs

--- ML_Process/app.py
+++ ML_Process/app.py
@@ -1,53 +1,29 @@
-# Load libraries
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-import pandas as pd
 import numpy as np
-import tensorflow as tf
-import keras
-import os
-from keras.models import load_model
-import sys
+from flask import Flask, request, jsonify, render_template
+import pickle
 
-# instantiate flask 
 app = Flask(__name__)
-
-
-# we need to redefine our metric function in order 
-# to use it when loading the model 
-def auc(y_true, y_pred):
-    auc = tf.metrics.auc(y_true, y_pred)[1]
-    keras.backend.get_session().run(tf.local_variables_initializer())
-    return auc
-
-# load the model, and pass in the custom metric function
-#global graph
-#graph = tf.get_default_graph()
-name = os.path.join('NBA.h5')
-print (os.getcwd())
-model = load_model(name)
-
-
-# define a predict function as an endpoint 
+model = pickle.load(open('finalized_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('NBATemplate.html')
+    return render_template('index.html')
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    int_features = [(x) for x in request.form.values()]
-    final_features = np.array(int_features)
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
+
     output = round(prediction[0], 2)
-    return render_template('machinelearning.html', prediction_text=output)
 
-@app.route('/performance')
-def performance():
-    return render_template('performance.html')
+    return render_template('machinelearning.html', prediction_text='The player will be {} MVP'.format(output))
 
-@app.route('/efficiency')
-def efficiency():
-    return render_template('efficiency.html')
+@app.route('/plots')
+def plots():
+
+    return render_template('Plots_Javascript\index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
